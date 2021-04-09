974b6fe3276cceebea53d0e122b838af9e008f39
Harshit Jain <51118591+HarshitDolu@users.noreply.github.com>
2021-04-10 05:17:12 +0530

    Update settings.py

--- creditCard/ccf/ccf/settings.py
+++ creditCard/ccf/ccf/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!uowzny-e_p-p6il!&=(bdg^+ia14yvjemqf_23(v5^eot6ps@'
+SECRET_KEY = "Add your secret key here"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,5 +137,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER="cipherx2510@gmail.com"
-EMAIL_HOST_PASSWORD="cipherx@2000"
+EMAIL_HOST_USER="abc@gmail.com"
+EMAIL_HOST_PASSWORD="abcdef"
